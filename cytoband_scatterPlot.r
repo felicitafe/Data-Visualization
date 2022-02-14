8c6f4ec2796fa08314a1c7daf3b6892ceebbe962
felicitafe <86530586+felicitafe@users.noreply.github.com>
2022-02-14 19:36:59 +0300

    Update cytoband_scatterPlot.r

--- cytoband_scatterPlot.r
+++ cytoband_scatterPlot.r
@@ -4,16 +4,16 @@
 
 ggplot(ex, aes(x=pos, y=pval), geom_jitter()) + 
   geom_point(stat = "identity",  size=.1, aes(colour = factor(label))) + 
-  xlab("Genomic Position") +
-  ylab("Adjusted p-value") +
+  xlab("cytoband") +
+  ylab("p-value") +
   ggtitle("ERα MCF7 Peaks") +
   theme(plot.title=element_text(hjust=0.5) ,
         axis.text.x=element_blank(),
         axis.ticks.x=element_blank()) +
   xlab("Genomic Position (chr1q)") +
   facet_grid(. ~ facet , scales = "free_x", space = "free_x", switch="both") +
-  scale_color_manual(name="Binding",
-                     labels=c("direct","indirect"),
+  scale_color_manual(name="label",
+                     labels=c("x","y"),
                      values=c("orangered","dodgerblue2"))  
 
 
